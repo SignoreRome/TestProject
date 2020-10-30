50a36c0f80b4eb0d7e5f473a59987dca6fdfb9b8
Roman Volobuev <BroChilling@yandex.ru>
2020-10-30 17:43:47 +0700

    added new class

---
+++ src/com/company/Human.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.List;
+
+public class Human {
+    private final String name;
+    private final List<String> pets;
+
+    public Human(String name, List<String> pets) {
+        this.name = name;
+        this.pets = pets;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getPets() {
+        return pets;
+    }
+}
